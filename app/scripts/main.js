835b066a40b0d524944ab2bd0e69f091b5919fea
Sergio Álvarez <sergioalvarezsuarez@gmail.com>
2015-04-20 23:19:27 +0200

    Basic skeleton by using Yeoman generator

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global B2pick, $*/
+
+
+window.B2pick = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    B2pick.init();
+});
