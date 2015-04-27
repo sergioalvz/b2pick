c7cf464fe35695d9a8440917ee32ef269f3fb0ee
Sergio Álvarez <sergioalvarezsuarez@gmail.com>
2015-04-28 01:42:24 +0200

    Reduce the indent size (from 4 to 2) and update dependencies

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,17 @@
 /*global B2pick, $*/
 
-
 window.B2pick = {
-    Models: {},
-    Collections: {},
-    Views: {},
-    Routers: {},
-    init: function () {
-        'use strict';
-        console.log('Hello from Backbone!');
-    }
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  init: function () {
+    'use strict';
+    console.log('Hello from Backbone!');
+  }
 };
 
 $(document).ready(function () {
-    'use strict';
-    B2pick.init();
+  'use strict';
+  B2pick.init();
 });
