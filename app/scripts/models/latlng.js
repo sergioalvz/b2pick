c08d1dc91be73be5a4caf3e98efeea10245daedd
Sergio Álvarez <sergioalvarezsuarez@gmail.com>
2015-04-28 02:32:06 +0200

    Setup some dummy tests

---
+++ app/scripts/models/latlng.js
@@ -0,0 +1,9 @@
+/*global B2pick, Backbone*/
+
+B2pick.Models = B2pick.Models || {};
+
+(function () {
+  'use strict';
+
+  B2pick.Models.LatLng = Backbone.Model.extend({});
+})();
---
+++ test/models/latlng.spec.js
@@ -0,0 +1,23 @@
+/*global beforeEach, describe, it, assert, expect  */
+'use strict';
+
+describe('LatLng Model', function () {
+  beforeEach(function () {
+    this.LatLngModel = new B2pick.Models.LatLng({
+      lat: 3,
+      lng: 5
+    });
+  });
+
+  describe("#initialize", function() {
+    it("builds the object correctly", function() {
+      var expectedLat = 3;
+      var expectedLng = 5;
+      var lat = this.LatLngModel.get("lat");
+      var lng = this.LatLngModel.get("lng");
+
+      lat.should.equal(expectedLat);
+      lng.should.equal(expectedLng);
+    });
+  });
+});
